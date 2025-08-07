3d0ed70cb681735ecb74b61db1bbed5ea59e5026
manolissyn <syntetas.m@gmail.com>
2025-08-07 16:51:17 +0300

    1 day trial

--- lib/website/screens/pricing_v2.dart
+++ lib/website/screens/pricing_v2.dart
@@ -1282,7 +1282,7 @@ class _SaasPricingCard extends StatelessWidget {
                                   borderRadius: BorderRadius.circular(10),
                                 ),
                                 child: Text(
-                                  '14 day free trial',
+                                  '1 day free trial',
                                   style: TextStyle(
                                     color: accentColor,
                                     fontWeight: FontWeight.bold,
